3e3acc36c76d4623feace20b2935a22a6d127ec1
Sibayan Misra <sibayanmisra2002@gmail.com>
2023-04-30 12:30:20 +0530

    Update Accounts.java

--- Assignment 6/Accounts.java
+++ Assignment 6/Accounts.java
@@ -1,11 +1,17 @@
-import java.util.*; abstract class Accounts {
+import java.util.*; 
+abstract class Accounts {
 double balance;
-String accountNumber, accountHoldersName, address; Accounts(double bal, String an, String name, String add) {
+String accountNumber, accountHoldersName, address; 
+ Accounts(double bal, String an, String name, String add) {
  
-balance = bal; accountNumber = an; accountHoldersName = name; address = add;
+balance = bal; 
+  accountNumber = an; 
+  accountHoldersName = name; 
+  address = add;
 }
 
-abstract void withdrawl(double amt); abstract void deposit(double amt);
+abstract void withdrawl(double amt); 
+abstract void deposit(double amt);
 void display() { System.out.println(
 "Balance of Account No( " + accountNumber + " ) = " + balance
 );
@@ -13,38 +19,55 @@ void display() { System.out.println(
 }
 
 class SavingsAccount extends Accounts { double rateOfInterest;
-SavingsAccount(double bal, String an, String name, String add, double rate) { super(bal, an, name, add);
-rateOfInterest = rate;
+SavingsAccount(double bal, String an, String name, String add, double rate) { 
+ super(bal, an, name, add);
+ rateOfInterest = rate;
 }
 
 public void withdrawl(double amt) { if (amt <= balance) {
-System.out.println("Withdrawal Successful"); balance -= amt;
-display();
-} else {
-System.out.println("Requested amount is greater than Balance"); display();
+ System.out.println("Withdrawal Successful"); 
+ balance -= amt;
+ display();
+} 
+else {
+ System.out.println("Requested amount is greater than Balance"); 
+ display();
 }
 }
 
-public void deposit(double amt) { System.out.println("Withdrawal Successful"); balance += amt;
-display();
+public void deposit(double amt) { 
+ System.out.println("Withdrawal Successful"); 
+ balance += amt;
+ display();
 }
 
 void calculateAmount() {
-double amt = (balance + balance * rateOfInterest / 100.0); System.out.println("Amount = " + amt);
-balance = amt; System.out.println("Balance Updated"); display();
+ double amt = (balance + balance * rateOfInterest / 100.0); 
+ System.out.println("Amount = " + amt);
+ balance = amt; 
+ System.out.println("Balance Updated"); 
+ display();
 }
 }
 
 public class program10 {
 
-public static void main(String args[]) { Scanner in = new Scanner(System.in); double bal, rate;
-String an, name, add; System.out.println("Enter Account Number: "); an = in.next();
-in.nextLine();
-System.out.println("Enter Account Holder Name: "); name = in.nextLine();
-System.out.println("Enter Address: "); add = in.nextLine();
-System.out.println("Enter Starting Balance: "); bal = in.nextDouble();
+public static void main(String args[]) { 
+ Scanner in = new Scanner(System.in); 
+ double bal, rate;
+String an, name, add; 
+ System.out.println("Enter Account Number: "); 
+ an = in.next();
+ in.nextLine();
+System.out.println("Enter Account Holder Name: "); 
+name = in.nextLine();
+System.out.println("Enter Address: "); 
+ add = in.nextLine();
+System.out.println("Enter Starting Balance: "); 
+bal = in.nextDouble();
  
-System.out.println("Enter Rate Of Interest: "); rate = in.nextDouble();
+System.out.println("Enter Rate Of Interest: "); 
+ rate = in.nextDouble();
 SavingsAccount ob = new SavingsAccount(bal, an, name, add, rate); System.out.println("Bank Account Successfully Created");
 int ch;
 w:while (true) { System.out.println(
